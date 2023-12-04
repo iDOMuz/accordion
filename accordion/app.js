fd0beac756c7d82609cdfe6549d4a337e7270ac4
iDOMuz <pubgaceking@gmail.com>
2023-12-04 11:26:47 +0500

    accordion
    accordion

---
+++ accordion/app.js
@@ -0,0 +1,29 @@
+let toggles = document.getElementsByClassName('toggle');
+let contentDiv = document.getElementsByClassName('content');
+let icons = document.getElementsByClassName('icon');
+
+for(let i=0; i<toggles.length; i++){
+    toggles[i].addEventListener('click', ()=>{
+        if( parseInt(contentDiv[i].style.height) != contentDiv[i].scrollHeight){
+            contentDiv[i].style.height = contentDiv[i].scrollHeight + "px";
+            toggles[i].style.color = "#0084e9";
+            icons[i].classList.remove('bx-plus');
+            icons[i].classList.add('bx-minus');
+        }
+        else{
+            contentDiv[i].style.height = "0px";
+            toggles[i].style.color = "#111130";
+            icons[i].classList.remove('bx-minus');
+            icons[i].classList.add('bx-plus');
+        }
+
+        for(let j=0; j<contentDiv.length; j++){
+            if(j!==i){
+                contentDiv[j].style.height = "0px";
+                toggles[j].style.color = "#111130";
+                icons[j].classList.remove('bx-minus');
+                icons[j].classList.add('bx-plus');
+            }
+        }
+    });
+}
